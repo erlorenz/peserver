25d4d8530f3928d96d9c4faa9632e95996cc2a58
Erik Lorenz <erikslorenz@gmail.com>
2018-12-11 02:02:28 -0800

    got dotenv working

--- src/server.js
+++ src/server.js
@@ -2,12 +2,14 @@ import express from 'express';
 import cors from 'cors';
 import helmet from 'helmet';
 import morgan from 'morgan';
-import config from 'config';
 import connectToDB from './startup/db';
 import configureWinston from './startup/logging';
 import apolloServer from './graphql/schema';
-
 import winston from 'winston';
+
+import dotenv from 'dotenv';
+dotenv.config();
+console.log(process.env.NODE_ENV);
 
 const app = express();
 
@@ -30,8 +32,7 @@ apolloServer.applyMiddleware({ app });
 const port = process.env.PORT || 3001;
 app.listen(port, () => {
   winston.info(
-    `Express running... port: ${port}, Environment: ${config.get(
-      'environment',
+    `Express running... port: ${port}, Environment: ${process.env.NODE_ENV},
     )},
     🚀 Server ready at http://localhost:${port}${apolloServer.graphqlPath}`,
   );

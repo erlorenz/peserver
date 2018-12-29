4cdc2ce3e583910e50e6b77a298eb0d9e5f3a2d5
Erik Lorenz <erikslorenz@gmail.com>
2018-12-29 03:21:23 -0800

    got app engine and db working for staging

--- knexfile.js
+++ knexfile.js
@@ -1,9 +1,31 @@
+// Update with your config settings.
 require('dotenv').config();
 
+const { DB_NAME, DB_PASSWORD, DB_USER, DB_SOCKET_NAME } = process.env;
+
 module.exports = {
-  client: 'pg',
-  connection: process.env.DB_URI,
-  migrations: {
-    tableName: 'knex_migrations',
+  staging: {
+    client: 'pg',
+    connection: {
+      database: DB_NAME,
+      user: DB_USER,
+      password: DB_PASSWORD,
+      host: `/cloudsql/${DB_SOCKET_NAME}`,
+    },
+    migrations: {
+      tableName: 'knex_migrations',
+    },
+  },
+
+  development: {
+    client: 'pg',
+    connection: {
+      database: DB_NAME,
+      user: DB_USER,
+      password: DB_PASSWORD,
+    },
+    migrations: {
+      tableName: 'knex_migrations',
+    },
   },
 };
--- src/db/index.js
+++ src/db/index.js
@@ -3,17 +3,19 @@ import { dbURI } from '../config/keys';
 import { Model } from 'objection';
 import AdminUser from '../models/AdminUser';
 import winston from 'winston';
+import knexConfig from '../../knexfile';
 
-const config = {
-  client: 'pg',
-  connection: dbURI,
-};
+// Decide which Knex config to use based on Env
+let config = knexConfig.development;
+if (process.env.NODE_ENV === 'production') config = knexConfig.staging;
 
+// Initialize Knex and Objection
 export default async () => {
   const knex = Knex(config);
 
   Model.knex(knex);
 
+  // Test if DB works by a simple request
   (async () => {
     try {
       await AdminUser.query();

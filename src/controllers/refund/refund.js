45feed347f6f3d56af26ebd3de1a86b1dafb29e7
Erik Lorenz <erikslorenz@gmail.com>
2019-01-16 12:24:35 -0800

    trying to figure out cors

--- src/controllers/refund/refund.js
+++ src/controllers/refund/refund.js
@@ -7,8 +7,6 @@ import { checkAuth } from '../../utils';
 export default async (payload, { models, currentUser }) => {
   checkAuth(currentUser);
 
-  console.log(payload);
-
   // Validate data
   validate(payload);
 

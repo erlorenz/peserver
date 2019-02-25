7826ea0c1c02ee0c9116c0453a2d77775e2b5df3
Erik Lorenz <erikslorenz@gmail.com>
2019-02-25 12:58:55 -0800

    changed text messages

--- src/services/twilio/messages.js
+++ src/services/twilio/messages.js
@@ -1,12 +1,12 @@
-const processedText = `Hi this is Press Express. We received your order... check your email for the receipt and instructions!`;
+const processedText = `This is Press Express - we received your order! Make sure to drop your garments at the bell desk by the scheduled pickup time. Call us at 702-620-3315 or email us at support@pressexpresslv.com with any questions.`;
 
-const pickedUpText = `Update from Press Express: Your items have been picked up!`;
+const pickedUpText = `Update from Press Express: Your items have been picked up.`;
 
 const outForDeliveryText =
-  'Update from Press Express: Your items are with the driver and will be delivered soon!';
+  'Update from Press Express: Your items are with the driver and will be delivered soon.';
 
 const completedText =
-  'Your items have been dropped off. Thank you for choosing Press Express!';
+  'Your items have been dropped off at the same location they were picked up. Thank you for choosing Press Express!';
 
 export const textBody = {
   processed: processedText,

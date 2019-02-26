ad86a95afa8f929f6a9c488aab4045e0bad16d9e
Erik Lorenz <erikslorenz@gmail.com>
2019-02-26 11:16:28 -0800

    making email template

--- src/controllers/checkout/dbTransaction.js
+++ src/controllers/checkout/dbTransaction.js
@@ -5,30 +5,24 @@ import { DateTime } from 'luxon';
 export default async payload => {
   let trx;
 
-  const orderFields = { ...payload };
-
   // Format Timestamps into ISO for Postgres (turn string to number)
-  orderFields.pickup_date = DateTime.fromMillis(+payload.pickup_date).toISO();
-  orderFields.return_date = DateTime.fromMillis(+payload.return_date).toISO();
+  payload.pickup_date = DateTime.fromMillis(+payload.pickup_date).toISO();
+  payload.return_date = DateTime.fromMillis(+payload.return_date).toISO();
 
-  console.log(orderFields.pickup_date);
   try {
     // Begin Transaction
     trx = await transaction.start(CustomerOrder.knex());
 
-    await CustomerOrder.query(trx).insertGraph(orderFields);
-    console.log('[transaction entered]');
+    await CustomerOrder.query(trx).insertGraph(payload);
 
     // Commit Transaction
     await trx.commit();
-    console.log('[Transaction committed]');
 
     return {
       success: true,
       message: 'Successfully entered into database.',
     };
   } catch (e) {
-    console.log('[NODE ERROR IN INSERT TO DB]', e);
     // Rollback transaction on error
     await trx.rollback();
 
